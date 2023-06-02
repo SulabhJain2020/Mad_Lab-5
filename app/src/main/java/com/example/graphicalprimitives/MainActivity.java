f3a20d6a5c54d6e94ab5d6d8631d59a3b2eb7fe6
SulabhJain2020 <yjain0245@gmail.com>
2023-06-02 10:49:14 +0530

    Mad Lab 5

---
+++ app/src/main/java/com/example/graphicalprimitives/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.graphicalprimitives;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Creating a Bitmap
+        Bitmap bg = Bitmap.createBitmap(720, 1280,
+                Bitmap.Config.ARGB_8888);
+        //Setting the Bitmap as background for the ImageView
+        ImageView i = (ImageView) findViewById(R.id.ImageView);
+        i.setBackgroundDrawable(new BitmapDrawable(bg));
+        //Creating the Canvas Object
+        Canvas canvas = new Canvas(bg);
+        //Creating the Paint Object and set its color & TextSize
+        Paint paint = new Paint();
+        Paint paint1 = new Paint();
+        paint.setColor(Color.BLACK);
+        paint1.setColor(Color.RED);
+        paint1.setTextSize(50);
+        //To draw a Rectangle
+        canvas.drawText("Rectangle", 420, 150, paint1);
+        canvas.drawRect(400, 200, 650, 700, paint);
+        //To draw a Circle
+        canvas.drawText("Circle", 120, 150, paint1);
+        canvas.drawCircle(200, 350, 150, paint);
+        //To draw a Square
+        canvas.drawText("Square", 120, 800, paint1);
+        canvas.drawRect(50, 850, 350, 1150, paint);
+        //To draw a Line
+        canvas.drawText("Line", 480, 800, paint1);
+        canvas.drawLine(520, 850, 520, 1150, paint);
+
+    }
+}
